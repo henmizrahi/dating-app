1d33167956294ed7c54185d46e59e95f9ad42361
Next EngGen4 <nextenggen4@Nexts-MacBook-Pro.local>
2020-01-15 16:15:16 +0200

    home page

---
+++ src/homePage/homePage.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import "./homePage.css"
+
+
+
+export function HomePage(){
+    return(
+        <div className = "HomePage">
+            <h2>Let find you a match..</h2>
+        </div>
+    )
+}
